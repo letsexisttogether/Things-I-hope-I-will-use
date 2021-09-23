453e01e094eea21cf78cee580126c0236f973e63
letsexisttogether <71719320+letsexisttogether@users.noreply.github.com>
2021-09-23 20:24:34 +0300

    Update digits_cutter.h

--- digits_cutter.h
+++ digits_cutter.h
@@ -3,22 +3,16 @@
 double cutter(double number, int symbsCount)
 {
     std::string taker = "", str = std::to_string(number);
-    int counter = 0;
     for (int i = 0; i < str.size(); i++)
     {
         taker += str[i];
         if (str[i] == '.')
         {
+            if (symbsCount > str.size() - i - 1)
+                return number;
             for (int j = i + 1; j < i + 1 + symbsCount; j++)
-            {
-                if (str.size() - counter < j)
-                    break;
                 taker += str[j];
-            }
-            break;
+            return std::atof(taker.c_str());
         }
-        counter++;
     }
-    number = std::atof(taker.c_str());
-    return number;
 }
